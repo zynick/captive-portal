db683e7a1ddb5cd6c4a147189cadb78d258ee5e0
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-14 21:20:32 +0800

    switch var to const

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var routes = require('./routes/index');
-var home = require('./routes/home');
-var login = require('./routes/login');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const logger = require('morgan');
+// const favicon = require('serve-favicon');
+const path = require('path');
 
-var app = express();
+const routes = require('./routes/index');
+const home = require('./routes/home');
+const login = require('./routes/login');
 
-// view engine setup
+const app = express();
+const isProd = process.env.NODE_ENV === 'production';
+
+
+/* View Engine Setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,35 +30,24 @@ app.use('/', routes);
 app.use('/home', home);
 app.use('/login', login);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+
+/* Catch 404 and Forward to Error Handler */
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+/* Error Handlers */
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
+    // only print stacktrace in development, hide in production
+    err = isProd ? {} : err;
     res.render('error', {
-      message: err.message,
-      error: err
+        message: err.message,
+        error: err
     });
-  });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
 
 
