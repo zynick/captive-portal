ffc3357adc462d95e590004662ade66b17003f9e
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-23 17:49:19 +0800

    use CHAP for Mikrotik AP

--- routes/login.js
+++ routes/login.js
@@ -5,12 +5,26 @@ const router = require('express').Router();
 
 /**
  * http://wiki.mikrotik.com/wiki/HotSpot_external_login_page
+ *
+ * NOTE: no way to use PAP auth in mikrotik because
+ * can't find the decoding method online. Plus it's not secure
+ * so better use CHAP instead.
  */
 
-router.post('/', (req, res) => {
-    res.render('login', { data: req.body });
-});
+router.post('/', (req, res, next) => {
 
+    const chapId = req.body['chap-id'];
+    const isCHAP = chapId && chapId.length > 0 ? true : false;
+
+    if (isCHAP) {
+        res.render('login', { data: req.body });
+    } else {
+        const err = new Error('Only CHAP Authentication Protocol is allowed.');
+        err.status = 500;
+        next(err);
+    }
+
+});
 
 
 module.exports = router;
