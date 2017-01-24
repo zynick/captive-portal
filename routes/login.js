fd2692423527bef00f422342ded092e521426dcb
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-24 12:32:18 +0800

    parsing mikrotik challenge to hex

--- routes/login.js
+++ routes/login.js
@@ -11,19 +11,35 @@ const router = require('express').Router();
  * so better use CHAP instead.
  */
 
+// input: "\001\002\377" output: "0102FF"
+function convertOctalString2Hex(octalString) {
+    let hex = '';
+    octalString
+        .substr(1)
+        .split('\\')
+        .forEach(str) {
+            let _hex = parseInt(str, 8).toString(16);
+            hex += _hex.length > 1 ? _hex : '0' + _hex;
+        }
+    return hex;
+}
+
 router.post('/', (req, res, next) => {
 
-    const chapId = req.body['chap-id'];
-    const isCHAP = chapId && chapId.length > 0 ? true : false;
+    // chap-id not important it's MikroTik's proprietary attribute
+    let chapId = req.body['chap-id'];
+    let chapChallenge = req.body['chap-challenge'];
 
-    if (isCHAP) {
-        res.render('login', { data: req.body });
-    } else {
+    if (!challenge || challenge.length !== 64) {
         const err = new Error('Only CHAP Authentication Protocol is allowed.');
-        err.status = 500;
+        err.status = 400;
         next(err);
     }
 
+    chapId = convertOctalString2Hex(chapId);
+    chapChallenge = convertOctalString2Hex(chapChallenge);
+
+    res.render('login', { data: req.body, chapId, chapChallenge });
 });
 
 
