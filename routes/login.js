9ec34f9bd0314fc636496cf29375a0006d370630
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-25 14:08:53 +0800

    refactor

--- routes/login.js
+++ routes/login.js
@@ -1,53 +1,17 @@
 'use strict';
 
-const log = require('debug')('cp:routes:login');
 const router = require('express').Router();
 
 /**
  * http://wiki.mikrotik.com/wiki/HotSpot_external_login_page
- *
- * NOTE: no way to use PAP auth in mikrotik because
- * can't find the decoding method online. Plus it's not secure
- * so better use CHAP instead.
  */
 
-// const oct2hex = (octalString) => {
-//     // input: "\001\002\377" output: "0102FF"
-//     let hex = '';
-//     octalString
-//         .substr(1)
-//         .split('\\')
-//         .forEach((str) => {
-//             let _hex = parseInt(str, 8).toString(16);
-//             hex += _hex.length > 1 ? _hex : '0' + _hex;
-//         });
-//     return hex;
-// };
-
 router.get('/', (req, res, next) => {
-    log('QUERY:', JSON.stringify(req.query, null, 2));
     res.render('login', { data: req.query });
 });
 
 router.post('/', (req, res, next) => {
-
-    // chap-id not important it's MikroTik's proprietary attribute
-    // let chapId = req.body['chap-id'];
-    // let chapChallenge = req.body['chap-challenge'];
-
-    // if (!chapChallenge || chapChallenge.length !== 64) {
-    //     const err = new Error('Only CHAP Authentication Protocol is allowed.');
-    //     err.status = 400;
-    //     next(err);
-    // }
-
-    log('BODY:', JSON.stringify(req.body, null, 2));
     res.render('login', { data: req.body });
-
-    // chapId = oct2hex(chapId);
-    // chapChallenge = oct2hex(chapChallenge);
-    // res.render('login', { data: req.body, chapId, chapChallenge });
-
 });
 
 
