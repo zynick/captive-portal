77fa73c1cc21587586f0085c0a1a95e9bf4a0394
Zi-Yuan Chan <ziyuan@zynick.com>
2017-02-06 14:45:58 +0800

    spacing

--- config.js
+++ config.js
@@ -3,9 +3,11 @@
 const {
     AD_HOST = 'connect-dev-api.tideanalytics.com',
     AD_KEY = 'DuS6YR9aCMJc0345mPNAkAIzaSyCChtVm4UlxD4',
+
     MONGO_HOST = 'localhost',
     MONGO_PORT = 27017,
     MONGO_DATABASE = 'radius',
+
     NODE_ENV = 'development',
     PORT = 3000
 } = process.env;
