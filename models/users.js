a94e5832078e08665675fdc031ab710fa7b28328
Zi-Yuan Chan <ziyuan@zynick.com>
2017-02-14 17:09:09 +0800

    refactor

--- models/users.js
+++ models/users.js
@@ -9,13 +9,13 @@ const userSchema = new Schema({
 
     username: {
         type: String,
-        required: 'Username does not exist',
+        required: 'Username is required.',
         index: true
     },
 
     organization: {
         type: String,
-        required: 'Organization does not exist'
+        required: 'Organization is required.'
     },
 
     password: String,
@@ -36,7 +36,7 @@ userSchema.index({ organization: 1, username: 1 }, { unique: 'Username already e
 userSchema.plugin(uniqueValidator);
 
 // Error Handling Middleware
-userSchema.post('save', (err, doc, next) => {
+const errorHandler = (err, doc, next) => {
     // TODO refactor: what will happen if only check and change via err.name === 'ValidationError'?
     err.originalMessage = err.message;
     const keys = Object.keys(err.errors);
@@ -45,12 +45,7 @@ userSchema.post('save', (err, doc, next) => {
     next(err);
 });
 
-userSchema.post('update', (err, doc, next) => {
-    err.originalMessage = err.message;
-    const keys = Object.keys(err.errors);
-    err.message = keys[0] ? err.errors[keys[0]].message : err.message;
-    err.status = 400;
-    next(err);
-});
+userSchema.post('save', errorHandler);
+userSchema.post('update', errorHandler);
 
 mongoose.model('Users', userSchema);
