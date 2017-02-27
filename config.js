e7141085546a6f2b6bb4303a09df632d4891228e
Zi-Yuan Chan <ziyuan@zynick.com>
2017-02-27 16:44:51 +0800

    admanager debug logging

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const {
-  AD_HOST = 'connect-dev-api.tideanalytics.com',
+  AD_HOST = 'manager-api-dev.tideanalytics.com',
   AD_KEY = 'DuS6YR9aCMJc0345mPNAkAIzaSyCChtVm4UlxD4',
   API_TOKEN = 'T9C6CXztdVhOdAc2azRdBfZrNW1FrHOJ',
   MONGO = 'localhost:27017/radius',
