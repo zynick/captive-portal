4f183467b508f28fa58e7bb7d002a3b557dd4803
1vt <ivan@tideanalytics.com>
2017-03-17 18:22:08 +0800

    errors

--- models/worker.js
+++ models/worker.js
@@ -1,14 +1,37 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose        = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const Schema          = mongoose.Schema;
 
 const workerSchema = new Schema({
 
   type:          {type: String, index: true},
   lastActivated: {type: Date, index: true, default: Date.now},
-  identifier:    String,
+
+  identifier: {
+    type:     String,
+    required: "this is required",
+    index:    true,
+    unique:   "already exist"
+  },
 
 });
+
+workerSchema.plugin(uniqueValidator);
+
+const errorHandler = (err, doc, next) => {
+
+  err.originalMessage = err.message;
+
+  const keys = Object.keys(err.errors);
+  err.message = keys[0] ? err.errors[keys[0]].message : err.message;
+  err.status = 400;
+
+  next(err);
+};
+
+workerSchema.post('save',   errorHandler);
+workerSchema.post('update', errorHandler);
 
 mongoose.model('Worker', workerSchema);
