14037911fef199dc0e2ff958332396c76baee830
Zi-Yuan Chan <ziyuan@zynick.com>
2017-03-21 17:13:49 +0800

    chapId and chapChallenge need to "binary" it

--- controllers/ap/mikrotik.js
+++ controllers/ap/mikrotik.js
@@ -18,7 +18,9 @@ const generateSuccessForm = (req, res, next) => {
   // prepare the form properly
   const { loginUrl, mac, redirectUrl, chapId, chapChallenge } = req.query;
   const { token, impressionUrl } = req.bag;
-  const password = chapId ? md5(`${chapId}${token}${chapChallenge}`) : token;
+  const chapIdBin = chapId.toString('binary');
+  const chapChallengeBin = chapChallenge.toString('binary');
+  const password = chapId ? md5(chapIdBin + token + chapChallengeBin) : token;
 
   const redirectForm = {
     url: loginUrl,
