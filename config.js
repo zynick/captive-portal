1382215559349c1a14ff54c9878ebc925b25e0e9
Zi-Yuan Chan <ziyuan@zynick.com>
2018-08-05 07:12:19 +0000

    well it doesn't matter anymore

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const {
-  AD_HOST = 'manager-api-dev.tideanalytics.com',
-  AD_KEY = 'DuS6YR9aCMJc0345mPNAkAIzaSyCChtVm4UlxD4',
-  HOST = 'https://captive-portal-dev.tideanalytics.com',
-  MONGO = 'localhost:27017/radius-dev',
+  AD_HOST = '',
+  AD_KEY = '',
+  HOST = '',
+  MONGO = '',
   NODE_ENV = 'development',
   PORT = 3000
 } = process.env;
